be3faa8d465cb855ed49913f05ad078c411d38bd
Cufee <vkouzin@outlook.com>
2022-04-04 17:53:25 -0400

    trying to fix legacy types

--- stats/v1/sessions.go
+++ stats/v1/sessions.go
@@ -8,28 +8,28 @@ import (
 
 // Session - Will be switching to this format soon
 type Session struct {
-	Vehicles      []statistics.VehicleStatsFrame `json:"vehicles" bson:"vehicles"`
-	Achievements  statistics.AchievementsFrame   `json:"achievements" bson:"achievements"`
-	PlayerID      int                            `json:"player_id" bson:"player_id"`
-	Timestamp     time.Time                      `json:"timestamp" bson:"timestamp"`
-	LastBattle    time.Time                      `json:"last_battle_time" bson:"last_battle_time"`
-	BattlesAll    int                            `json:"battles_random" bson:"battles_random"`
-	StatsAll      statistics.StatsFrame          `json:"stats_random" bson:"stats_random"`
-	BattlesRating int                            `json:"battles_rating" bson:"battles_rating"`
-	StatsRating   statistics.StatsFrame          `json:"stats_rating" bson:"stats_rating"`
-	SessionRating int                            `json:"session_wn8" bson:"session_wn8"`
+	Vehicles      []VehicleStats               `json:"vehicles" bson:"vehicles"`
+	Achievements  statistics.AchievementsFrame `json:"achievements" bson:"achievements"`
+	PlayerID      int                          `json:"player_id" bson:"player_id"`
+	Timestamp     time.Time                    `json:"timestamp" bson:"timestamp"`
+	LastBattle    time.Time                    `json:"last_battle_time" bson:"last_battle_time"`
+	BattlesAll    int                          `json:"battles_random" bson:"battles_random"`
+	StatsAll      statistics.StatsFrame        `json:"stats_random" bson:"stats_random"`
+	BattlesRating int                          `json:"battles_rating" bson:"battles_rating"`
+	StatsRating   statistics.StatsFrame        `json:"stats_rating" bson:"stats_rating"`
+	SessionRating int                          `json:"session_wn8" bson:"session_wn8"`
 }
 
 // RetroSession - Session using old data structure
 type RetroSession struct {
-	Vehicles      map[string]statistics.VehicleStatsFrame `json:"vehicles" bson:"vehicles"`
-	Achievements  statistics.AchievementsFrame            `json:"achievements" bson:"achievements"`
-	PlayerID      int                                     `json:"player_id" bson:"player_id"`
-	Timestamp     time.Time                               `json:"timestamp" bson:"timestamp"`
-	LastBattle    time.Time                               `json:"last_battle_time" bson:"last_battle_time"`
-	BattlesAll    int                                     `json:"battles_random" bson:"battles_random"`
-	StatsAll      statistics.StatsFrame                   `json:"stats_random" bson:"stats_random"`
-	BattlesRating int                                     `json:"battles_rating" bson:"battles_rating"`
-	StatsRating   statistics.StatsFrame                   `json:"stats_rating" bson:"stats_rating"`
-	SessionRating int                                     `json:"session_wn8" bson:"session_wn8"`
+	Vehicles      map[string]VehicleStats      `json:"vehicles" bson:"vehicles"`
+	Achievements  statistics.AchievementsFrame `json:"achievements" bson:"achievements"`
+	PlayerID      int                          `json:"player_id" bson:"player_id"`
+	Timestamp     time.Time                    `json:"timestamp" bson:"timestamp"`
+	LastBattle    time.Time                    `json:"last_battle_time" bson:"last_battle_time"`
+	BattlesAll    int                          `json:"battles_random" bson:"battles_random"`
+	StatsAll      statistics.StatsFrame        `json:"stats_random" bson:"stats_random"`
+	BattlesRating int                          `json:"battles_rating" bson:"battles_rating"`
+	StatsRating   statistics.StatsFrame        `json:"stats_rating" bson:"stats_rating"`
+	SessionRating int                          `json:"session_wn8" bson:"session_wn8"`
 }
