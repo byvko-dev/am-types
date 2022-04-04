39cb0706033c66a35452fceb2e6d8263d7b87f4f
Cufee <vkouzin@outlook.com>
2022-04-04 18:17:10 -0400

    added dataprep settings

---
+++ dataprep/v1/settings/options.go
@@ -0,0 +1,88 @@
+package settings
+
+type BlockOptions struct {
+	GenerationTag   string `json:"generationTag,omitempty"`
+	LocalizationTag string `json:"localizationTag,omitempty"`
+
+	IconDictOverwrite  map[string]string `json:"iconDictOverwrite,omitempty"`  // By default icons are arrows
+	IconColorOverWrite string            `json:"iconColorOverwrite,omitempty"` // By default, icons are red,green,yellow based on session vs all time comparison
+	HasIcon            bool              `json:"hasIcon,omitempty"`
+}
+
+func (b BlockOptions) WithIcon() BlockOptions {
+	b.HasIcon = true
+	return b
+}
+
+type Options struct {
+	Locale string `json:"locale"`
+
+	// Status icons
+	AccountStatus StatusIconsOptions `json:"accountStatus,omitempty" firestore:"accountStatus"`
+
+	// Notifications
+	Notifications NotificationsOptions `json:"notifications,omitempty" firestore:"notifications"`
+
+	// Challenges
+	Challenges ChallengesOptions `json:"challenges,omitempty" firestore:"challenges"`
+
+	// Player info and general stats
+	Player         PlayerOptions   `json:"player,omitempty" firestore:"player"`
+	RatingBattles  OverviewOptions `json:"ratingBattles,omitempty" firestore:"ratingBattles"`
+	RegularBattles OverviewOptions `json:"regularBattles,omitempty" firestore:"regularBattles"`
+
+	// Per vehicle stats
+	VehiclesFull VehicleOptions `json:"vehiclesFull,omitempty" firestore:"vehiclesFull"`
+	VehiclesSlim VehicleOptions `json:"vehiclesSlim,omitempty" firestore:"vehiclesSlim"`
+}
+
+type StatusIconsOptions struct {
+	Include bool `json:"include,omitempty" firestore:"include"`
+	Limit   int  `json:"limit,omitempty" firestore:"limit"`
+}
+
+type NotificationsOptions struct {
+	Include bool           `json:"include,omitempty" firestore:"include"`
+	Blocks  []BlockOptions `json:"blocks,omitempty" firestore:"blocks"`
+}
+
+type ChallengesOptions struct {
+	Include bool           `json:"include,omitempty" firestore:"include"`
+	Limit   int            `json:"limit,omitempty" firestore:"limit"`
+	Types   []string       `json:"types,omitempty" firestore:"types"`
+	Blocks  []BlockOptions `json:"blocks,omitempty" firestore:"blocks"`
+}
+
+type PlayerOptions struct {
+	Include     bool `json:"include,omitempty" firestore:"include"`
+	WithName    bool `json:"withName,omitempty" firestore:"withName"`
+	WithClanTag bool `json:"withClanTag,omitempty" firestore:"withClanTag"`
+	WithPins    bool `json:"withPins,omitempty" firestore:"withPins"`
+}
+
+type OverviewOptions struct {
+	Include          bool           `json:"include,omitempty" firestore:"include"`
+	WithIcons        bool           `json:"withIcons,omitempty" firestore:"withIcons"`
+	WithTitle        bool           `json:"withTitle,omitempty" firestore:"withTitle"`
+	WithLabels       bool           `json:"withLabels,omitempty" firestore:"withLabels"`
+	WithAllTimeStats bool           `json:"withAllTimeStats,omitempty" firestore:"withAllTimeStats"`
+	Type             string         `json:"type,omitempty" firestore:"type"`
+	Blocks           []BlockOptions `json:"blocks,omitempty" firestore:"blocks"`
+}
+
+type VehicleOptions struct {
+	Include          bool           `json:"include,omitempty" firestore:"include"`
+	Limit            int            `json:"limit,omitempty" firestore:"limit"`
+	WithVehicleTier  bool           `json:"withVehicleTier,omitempty" firestore:"withVehicleTier"`
+	WithVehicleName  bool           `json:"withVehicleName,omitempty" firestore:"withVehicleName"`
+	WithAllTimeStats bool           `json:"withAllTimeStats,omitempty" firestore:"withAllTimeStats"`
+	WithLabels       bool           `json:"withLabels,omitempty" firestore:"withLabels"`
+	WithIcons        bool           `json:"withIcons,omitempty" firestore:"withIcons"`
+	Offset           int            `json:"offset,omitempty" firestore:"offset"`
+	Blocks           []BlockOptions `json:"blocks,omitempty" firestore:"blocks"`
+}
+
+const (
+	OverviewTypeRegular = "regular"
+	OverviewTypeRating  = "rating"
+)
---
+++ dataprep/v1/settings/settings.go
@@ -0,0 +1,55 @@
+package settings
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type GenerationSettings struct {
+	ID               primitive.ObjectID `json:"id" firestore:"id" bson:"_id,omitempty"`
+	Options          Options            `json:"options,omitempty" firestore:"options" bson:"options"`
+	StylePreset      string             `json:"stylePreset,omitempty" firestore:"stylePreset" bson:"stylePreset"`
+	UseCustomOptions bool               `json:"useCustomOptions,omitempty" firestore:"useCustomOptions" bson:"useCustomOptions"`
+	LastUsed         time.Time          `json:"lastUsed,omitempty" firestore:"lastUsed" bson:"lastUsed"`
+
+	Player struct {
+		ID    int    `json:"id,omitempty" firestore:"id" bson:"id"`
+		Realm string `json:"realm,omitempty" firestore:"realm" bson:"realm"`
+	} `json:"player,omitempty" firestore:"player" bson:"player"`
+
+	OwnerId string `json:"ownerId,omitempty" firestore:"ownerId" bson:"ownerId"`
+	Locale  string `json:"locale,omitempty" firestore:"locale" bson:"locale"`
+}
+
+func (s *GenerationSettings) Validate() error {
+	if s.Player.ID == 0 {
+		return errors.New("player ID is not set")
+	}
+	if !CheckRealm(s.Player.Realm) {
+		return errors.New("realm is not valid")
+	}
+	var blank Options
+	if fmt.Sprintf("%v", s.Options) == fmt.Sprintf("%v", blank) {
+		return errors.New("options are not set")
+	}
+	return nil
+}
+
+func CheckRealm(realm string) bool {
+	switch realm {
+	case "NA":
+		return true
+	case "EU":
+		return true
+	case "RU":
+		return true
+	case "ASIA":
+		return true
+	default:
+		return false
+	}
+
+}
