93117fcb9f3b629eb4abfb453d6921149461b897
Cufee <vkouzin@outlook.com>
2022-10-20 16:17:55 -0400

    Added methods

--- stats/v3/frame.go
+++ stats/v3/frame.go
@@ -13,3 +13,37 @@ type CompleteFrame struct {
 	Regular Frame `json:"regular" bson:"regular"`
 	Rating  Frame `json:"rating" bson:"rating"`
 }
+
+func (f *Frame) Add(b *Frame) {
+	f.Total.Add(&b.Total)
+	f.Achievements.Add(&b.Achievements)
+	for key, value := range b.Ratings {
+		f.Ratings[key] += value
+	}
+	for key, value := range b.Vehicles {
+		if _, ok := f.Vehicles[key]; !ok {
+			f.Vehicles[key] = value
+		} else {
+			v := f.Vehicles[key]
+			v.Add(&value)
+			f.Vehicles[key] = v
+		}
+	}
+}
+
+func (f *Frame) Subtract(b *Frame) {
+	f.Total.Subtract(&b.Total)
+	f.Achievements.Subtract(&b.Achievements)
+	for key, value := range b.Ratings {
+		f.Ratings[key] -= value
+	}
+	for key, value := range b.Vehicles {
+		if _, ok := f.Vehicles[key]; !ok {
+			f.Vehicles[key] = value
+		} else {
+			v := f.Vehicles[key]
+			v.Subtract(&value)
+			f.Vehicles[key] = v
+		}
+	}
+}
