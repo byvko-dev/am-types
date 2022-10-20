cc5f3232cbbda768a890574c50df0f0ddd623a9f
Cufee <vkouzin@outlook.com>
2022-10-20 11:53:16 -0400

    Added support for localization

--- wargaming/v2/glossary/achievements.go
+++ wargaming/v2/glossary/achievements.go
@@ -1,10 +1,10 @@
 package glossary
 
 type AchievementDetails struct {
-	ID          string `json:"achievement_id" bson:"achievement_id"`
-	Name        string `json:"realm" bson:"name"`
-	Section     string `json:"section" bson:"section"`
-	ImageURL    string `json:"image" bson:"image"`
-	Description string `json:"description" bson:"description"`
-	Condition   string `json:"condition" bson:"condition"`
+	ID          string            `json:"achievement_id" bson:"achievement_id"`
+	Name        map[string]string `json:"realm" bson:"name"`
+	Section     string            `json:"section" bson:"section"`
+	ImageURL    string            `json:"image" bson:"image"`
+	Description string            `json:"description" bson:"description"`
+	Condition   string            `json:"condition" bson:"condition"`
 }
---
+++ wargaming/v2/glossary/languages.go
@@ -0,0 +1,19 @@
+package glossary
+
+const (
+	LangEN   = "en"    // English
+	LangRU   = "ru"    // Russian
+	LangPL   = "pl"    // Polish
+	LangDE   = "de"    // German
+	LangFR   = "fr"    // French
+	LangES   = "es"    // Spanish
+	LangTR   = "tr"    // Turkish
+	LangCS   = "cs"    // Czech
+	LangTH   = "th"    // Thai
+	LangKO   = "ko"    // Korean
+	LangVI   = "vi"    // Vietnamese
+	LangZhCH = "zh-cn" // Simplified Chinese
+	LangZhTW = "zh-tw" // Traditional Chinese
+)
+
+var AllLanguages = []string{LangEN, LangRU, LangPL, LangDE, LangFR, LangES, LangTR, LangCS, LangTH, LangKO, LangVI, LangZhCH, LangZhTW}
