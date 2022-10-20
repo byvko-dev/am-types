b2133c036be6856eb0681e64ec374e0dfbab529a
Cufee <vkouzin@outlook.com>
2022-10-20 14:39:08 -0400

    added ratings to snapshots

--- stats/v3/snapshots.go
+++ stats/v3/snapshots.go
@@ -6,9 +6,11 @@ type SnapshotStats struct {
 	Total        statistics.StatsFrame        `json:"total" bson:"total"`
 	Achievements statistics.AchievementsFrame `json:"achievements" bson:"achievements,omitempty"`
 	Vehicles     map[int]SnapshotVehicleStats `json:"vehicles" bson:"vehicles,omitempty"`
+	Ratings      map[string]int               `json:"ratings" bson:"ratings,omitempty"`
 }
 
 type SnapshotVehicleStats struct {
 	statistics.VehicleStatsFrame `bson:",inline"`
 	Achievements                 statistics.AchievementsFrame `json:"achievements" bson:"achievements,omitempty"`
+	Ratings                      map[string]int               `json:"ratings" bson:"ratings,omitempty"`
 }
