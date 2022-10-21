7546337e609aac3400b50974807a1d5ce99d1fcc
Cufee <vkouzin@outlook.com>
2022-10-21 15:31:47 -0400

    fixed inline

--- users/v2/externalprofiles.go
+++ users/v2/externalprofiles.go
@@ -6,10 +6,10 @@ const ExternalServiceWargaming ExternalService = "wargaming"
 const ExternalServiceDiscord ExternalService = "discord"
 
 type ExternalConnection struct {
-	ID string `bson:"_id,omitempty" json:"id"`
+	ID                string `bson:"_id,omitempty" json:"id"`
+	ExternalProfileID `bson:",inline" json:",inline"`
 
-	UserID string `json:"user_id" bson:"user_id"`
-	ExternalProfileID
+	UserID       string            `json:"user_id" bson:"user_id"`
 	AccessToken  string            `json:"access_token" firestore:"access_token" bson:"access_token"`
 	RefreshToken string            `json:"refresh_token" firestore:"refresh_token" bson:"refresh_token"`
 	Meta         map[string]string `json:"meta" firestore:"meta" bson:"meta"`
